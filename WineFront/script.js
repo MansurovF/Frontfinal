fd1425a5ecd2dea74086fb803ace1a98b79e8962
MansurovF <112488802+MansurovF@users.noreply.github.com>
2022-12-23 18:26:54 +0400

    Add files via upload

---
+++ WineFront/script.js
@@ -0,0 +1,443 @@
+
+
+
+
+$('.slides').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.champain').slick({
+  dots: true,
+  infinite: false,
+  speed: 2000,
+  arrowtoshow: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.slider3').slick({
+  dots: true,
+  infinite: true,
+  speed: 1200,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.slider3').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.slider3').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.slider3').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.slider4').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('#six').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.sliderr3').slick({
+  dots: false,
+  infinite: true,
+  speed: 900,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// class PriceRange extends HTMLElement {
+//   constructor() {
+//     super();
+
+//     console.log('Price Range: Constructor', this);
+//   }
+
+//   connectedCallback() {
+//     // Elements
+//     this.elements = {
+//       container: this.querySelector('div'),
+//       track: this.querySelector('div > div'),
+//       from: this.querySelector('input:first-of-type'),
+//       to: this.querySelector('input:last-of-type'),
+//       output: this.querySelector('output')
+//     }
+
+//     // Event listeners
+//     this.elements.from.addEventListener('input', this.handleInput.bind(this));
+//     this.elements.to.addEventListener('input', this.handleInput.bind(this));
+
+//     // Properties
+//     this.currency = (this.hasAttribute('currency') &&
+//       this.getAttribute('currency') !== undefined &&
+//       this.getAttribute('currency') !== '') ? this.getAttribute('currency') : '£';
+
+//     // Update the DOM
+//     this.updateDom();
+
+//     console.log('Price Range: Connected', this);
+//   }
+
+//   disconnectedCallback() {
+//     delete this.elements;
+//     delete this.currency;
+
+//     console.log('Price Range: Disconnected', this);
+//   }
+
+//   get from() {
+//     return parseInt(this.elements.from.value);
+//   }
+//   get to() {
+//     return parseInt(this.elements.to.value);
+//   }
+
+//   handleInput(event) {
+//     if (parseInt(this.elements.to.value) - parseInt(this.elements.from.value) <= 1) {
+//       if (event.target === this.elements.from) {
+//         this.elements.from.value = (parseInt(this.elements.to.value) - 1);
+//       } else if (event.target === this.elements.to) {
+//         this.elements.to.value = (parseInt(this.elements.from.value) + 1);
+//       }
+//     }
+
+//     // Update the DOM
+//     this.updateDom();
+
+//     console.log('Price Range: Updated!!', {
+//       from: parseInt(this.elements.from.value),
+//       to: parseInt(this.elements.to.value)
+//     });
+//   }
+
+//   updateDom() {
+//     this.drawFill();
+//     this.drawOutput();
+//   }
+
+//   drawFill() {
+//     const percent1 = (this.elements.from.value / this.elements.from.max) * 100,
+//       percent2 = (this.elements.to.value / this.elements.to.max) * 100;
+
+//     this.elements.track.style.background = `linear-gradient(to right, var(--track-color) ${percent1}%, var(--track-highlight-color) ${percent1}%, var(--track-highlight-color) ${percent2}%, var(--track-color) ${percent2}%)`;
+//   }
+
+//   drawOutput() {
+//     this.elements.output.textContent = `${this.currency}${this.elements.from.value} - ${this.currency}${this.elements.to.value}`;
+//   }
+// }
+
+
+
+
+// const review = document.querySelector(".review")
+// const reviewBtn = document.querySelector(".review-btn")
+// const inP=document.querySelector(".in-p")
+// const descBtn=document.querySelector(".desc-btn")
+// reviewBtn.addEventListener("click", function () {
+//   inP.classList.remove("show")
+//   inP.classList.add("none")
+//   review.classList.add("show")
+
+// })
+// descBtn.addEventListener("click",()=>{
+//   inP.classList.remove("none")
+//   inP.classList.add("show")
+//   review.classList.remove("show")
+//   review.classList.add("none")
+// })
+// $(document).ready(function() {
+//   $('.minus').click(function () {
+//     var $input = $(this).parent().find('input');
+//     var count = parseInt($input.val()) - 1;
+//     count = count < 1 ? 1 : count;
+//     $input.val(count);
+//     $input.change();
+//     return false;
+//   });
+//   $('.plus').click(function () {
+//     var $input = $(this).parent().find('input');
+//     $input.val(parseInt($input.val()) + 1);
+//     $input.change();
+//     return false;
+//   });
+// });
+
+
+
+
+
